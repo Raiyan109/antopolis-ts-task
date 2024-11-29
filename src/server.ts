3c368e685a832e5ebb42891a7f69c641ce14fca4
Raiyan109 <kraiyan109@gmail.com>
2024-11-29 11:54:46 +0600

    now deploy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,15 @@
 import mongoose from 'mongoose';
 import app from './app';
 import config from './app/config';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 async function main() {
     try {
         await mongoose.connect(config.database_url as string);
+        console.log(config.database_url);
+
 
         app.listen(config.port, () => {
             console.log(`app is listening on port ${config.port}`);
